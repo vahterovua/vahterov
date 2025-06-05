404826f9921914a2a60d1bddf5ed66d5877a4d4e
vahterovua <u9172778297@yandex.ru>
2025-06-05 12:09:18 +0300

    Update privet.py

--- privet.py
+++ privet.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.text('привет')
 st.text('приветики')
+
+url = 'https://storage.googleapis.com/s4a-prod-share-preview/default/default_github_user_logo.png' # ссылка на картинку
+st.image(url, caption='Картинка по ссылке')
