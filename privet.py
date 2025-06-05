da2f39cf2771182617e80a246a35d096eb131103
vahterovua <u9172778297@yandex.ru>
2025-06-05 12:13:16 +0300

    Update privet.py

--- privet.py
+++ privet.py
@@ -3,4 +3,7 @@ st.text('привет')
 st.text('приветики')
 
 url = 'https://avatars.mds.yandex.net/get-mpic/4881627/2a000001928b130814167dac8b895f6dbb17/optimize' # ссылка на картинку
-st.image(url, caption='Картинка по ссылке')
+st.image(url, caption='счетчик')
+
+st.image('https://avatars.mds.yandex.net/get-mpic/5284145/2a000001928b13081654a0a52d587a421cb6/optimize')
+
