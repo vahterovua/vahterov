1cf5ae5f739dad0df1a440d663af5e3a572bd7d6
vahterovua <u9172778297@yandex.ru>
2025-06-05 12:20:03 +0300

    Update privet.py

--- privet.py
+++ privet.py
@@ -7,3 +7,11 @@ st.image(url, caption='счетчик')
 
 st.image('https://avatars.mds.yandex.net/get-mpic/5284145/2a000001928b13081654a0a52d587a421cb6/optimize')
 
+
+
+
+import streamlit.components.v1 as components
+
+html_code = """ <iframe frameborder="0" src="https://datalens.yandex/4hvj54abmk68q?_no_controls=1&_theme=dark"></iframe> """
+
+components.html(html_code, height=200)
