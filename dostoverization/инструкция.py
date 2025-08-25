b01deddb79205c6fb19a592296193a00246345d3
vahterovua <u9172778297@yandex.ru>
2025-08-25 17:31:03 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -5,11 +5,7 @@ st.components.v1.html(
 width=2000, height=1500
 )
 
-st.components.v1.html(
-""" <div style="display: flex; justify-content: center; align-items: center; height: 100vh;"> <iframe style="width: 100%; max-width: 1200px; height: 800px; border: none;" src="https://datalens.yandex/xcn1i9hfrljqj?_theme=dark"> </iframe> </div> """,
-width=2000, height=1500
-)
-
+st.text('инструкция')
 
 
 
