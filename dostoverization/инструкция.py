d208bd65267830138cde2b20f9f656aa68b5c09c
vahterovua <u9172778297@yandex.ru>
2025-09-08 13:03:56 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -6,26 +6,5 @@ import streamlit as st
 
 st.text('инструкция')
 
-# Создадим поле выбора для даты
-show_timerange = st.sidebar.checkbox("Show date range")
-if show_timerange == True:
-    # Вычислим даты для создания временного слайдера
-    min_ts = min(df[DATE_COLUMN]).to_pydatetime()
-    max_ts = max(df[DATE_COLUMN]).to_pydatetime()
-    day_date = pd.to_datetime(st.sidebar.slider("Date to chose", min_value=min_ts, max_value=max_ts, value=max_ts))
-    st.write(f"Data for {day_date.date()}")
-    df = df[(df['date'] == day_date)]
-
-# Создадим поле выбора для визуализации общего количества случаев, смертей или вакцинаций
-select_event = st.sidebar.selectbox('Show map', ('cases per million', 'deaths per million', 'vaccinated per hundred'))
-if select_event == 'cases per million':
-    st.plotly_chart(draw_map_cases(), use_container_width=True)
-
-if select_event == 'deaths per million':
-    st.plotly_chart(draw_map_deaths(), use_container_width=True)
-
-if select_event == 'vaccinated per hundred':
-    st.plotly_chart(draw_map_vaccine(), use_container_width=True)
-
-
-
+# Отображение PDF с URL  
+st.pdf("https://example.com/document.pdf")
