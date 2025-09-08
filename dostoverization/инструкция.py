1653283a8d3e054c16367d8845b6b36563cb36f2
vahterovua <u9172778297@yandex.ru>
2025-09-08 12:51:46 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 st.components.v1.html(
-""" <div style="display: flex; justify-content: center; align-items: center; height: 100vh;"> <iframe style="width: 100%; max-width: 1200px; height: 800px; border: none;" src="https://datalens.yandex/xcn1i9hfrljqj?_theme=dark"> </iframe> </div> """,
-width=2000, height=1500
+""" <iframe frameborder="0" src="https://datalens.yandex/ctuxlru5a0jky?_no_controls=1"></iframe>
 )
 
 st.text('инструкция')
