709cef212322db0aa2a106d1006df5b464b3052a
vahterovua <u9172778297@yandex.ru>
2025-09-08 12:57:50 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.components.v1.html(
-""" <iframe frameborder="5" src="https://datalens.yandex/ctuxlru5a0jky?_no_controls=5"></iframe>"""
-)
+# st.components.v1.html(
+# """ <iframe frameborder="5" src="https://datalens.yandex/ctuxlru5a0jky?_no_controls=5"></iframe>"""
+# )
 
 st.text('инструкция')
 
