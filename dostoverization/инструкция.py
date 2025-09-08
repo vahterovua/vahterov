4026854c3be93344765e379c28010f33703934e3
vahterovua <u9172778297@yandex.ru>
2025-09-08 14:25:48 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -6,5 +6,3 @@ import streamlit as st
 
 st.text('инструкция')
 
-# Используем iFrame для показа PDF
-st.write(f'<iframe src="dostoverization/merkuriy-255-avlg-811-01-00-ps-2021-06-02.pdf" width="200%" height="1600px"></iframe>', unsafe_allow_html=True)
