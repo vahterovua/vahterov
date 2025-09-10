e2277e2a7f11f0e3788ae3f5def8266528549d13
vahterovua <u9172778297@yandex.ru>
2025-09-10 10:12:57 +0300

    Update конвертер-XML80020-Excel-сервер.py

--- dostoverization/конвертер-XML80020-Excel-сервер.py
+++ dostoverization/конвертер-XML80020-Excel-сервер.py
@@ -147,6 +147,7 @@ for file in os.listdir(folder_path):
 import plotly.express as px
 # Преобразование данных с помощью pivot()
 df_pivot = final_df.pivot(index='Дата_время', columns='measuring_point_code', values='value')
+st.markdown(f'общая таблица')
 df_pivot
 
 fig1 = px.bar(df_pivot,x=df_pivot.index,y=df_pivot.columns, title='графики потребления электроэнергии в кВт*ч')
