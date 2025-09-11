b1962c99bbcdaa085b8dfc301200d01b3bba535c
vahterovua <u9172778297@yandex.ru>
2025-09-11 14:18:38 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -8,12 +8,5 @@ from pathlib import Path
 st.title("Загрузчик файлов 📁")
 
 uploaded_file = st.file_uploader("Выберите файл для загрузки:", type=["txt", "md", "csv","mp3"])
-if uploaded_file is not None:
-    if st.button("Загрузить файл"):
-        success = upload_file_to_github(uploaded_file)
-        if success:
-            st.success(f"Успешно загружено: **{uploaded_file.name}**!")
-        else:
-            st.error("Что-то пошло не так.")
 
 
