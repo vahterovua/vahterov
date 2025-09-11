f2f88c565d8be0800c209fad491f6bfa637274e6
vahterovua <u9172778297@yandex.ru>
2025-09-11 14:22:01 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -9,4 +9,10 @@ st.title("Загрузчик файлов 📁")
 
 uploaded_file = st.file_uploader("Выберите файл для загрузки:", type=["txt", "md", "csv","mp3"])
 
-uploaded_file
+if uploaded_file is not None:
+    # Отображаем метаданные файла
+    file_details = {"Имя": uploaded_file.name, "Тип": uploaded_file.type, "Размер": uploaded_file.size}
+    st.write(file_details)
+    
+    # Создаем ссылку для скачивания загруженного файла
+    st.markdown(download_link(uploaded_file.getvalue().decode(), uploaded_file.name, 'Нажмите сюда, чтобы скачать файл'), unsafe_allow_html=True)
