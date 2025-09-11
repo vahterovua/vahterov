95fa4c800cf4c4d1fca57fa378072bf434fc02ef
vahterovua <u9172778297@yandex.ru>
2025-09-11 14:49:18 +0300

    Update инструкция.py

--- dostoverization/инструкция.py
+++ dostoverization/инструкция.py
@@ -7,6 +7,27 @@ uploaded_file = st.file_uploader("Выберите файл для загруз�
 
 
 
+def download_file(file_path):
+    """Генерирует ссылку для скачивания файла."""
+    with open(file_path, mode='rb') as file:
+        return file.read()
 
-# Получаем путь текущего каталога
-st.write(os.getcwd())
+# Получение списка всех файлов в текущей рабочей директории
+current_directory = os.getcwd()  # Текущий рабочий каталог
+all_files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
+
+if len(all_files) > 0:
+    st.subheader('Доступные файлы для скачивания:')
+    for file in all_files:
+        file_path = os.path.join(current_directory, file)
+        button_label = f"📥 {file}"
+        # Создаем кнопку для скачивания каждого файла
+        st.download_button(
+            label=button_label,
+            data=download_file(file_path),
+            file_name=file,
+            mime=None,  # Auto-detect MIME type based on the extension
+            key=file  # Уникальный ключ для каждой кнопки
+        )
+else:
+    st.info("Нет доступных файлов для скачивания.")
