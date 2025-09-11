d2a603f898d12b5a79b21c94de1a3ef1a1b00792
vahterovua <u9172778297@yandex.ru>
2025-09-11 12:43:12 +0300

    Update extract_text.py

--- pdf-text/extract_text.py
+++ pdf-text/extract_text.py
@@ -14,8 +14,7 @@ def extract_and_recognize_text(pdf_path, output_file, lang='eng'):
     Извлекает изображения из PDF и распознает текст с указанным языком.
 
     Параметры:
-    pdf_path (str): 'https://github.com/vahterovua/vahterov/blob/e934e8360405e393a735a8a88b2e4752ded808d6/pdf-text/%D0%A0%D1%83%D0%BA%D0%BE%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%20%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B3%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%20%D0%9A%D0%92%D0%90%D0%9D%D0%A2.pdf'
-    output_file (str): путь к выходному текстовому файлу
+    pdf_path (str): 
     lang (str): язык распознавания. Примеры:
         'eng' - английский
         'rus' - русский
@@ -61,7 +60,7 @@ def check_confidence(image, lang='rus'):
     return sum(confidences) / len(confidences) if confidences else 0
 
 # Для русского языка
-pdf_file = sys.argv[1]
+pdf_file = 'https://github.com/vahterovua/vahterov/blob/e934e8360405e393a735a8a88b2e4752ded808d6/pdf-text/%D0%A0%D1%83%D0%BA%D0%BE%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%20%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%20%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B3%D1%83%D1%80%D0%B0%D1%82%D0%BE%D1%80%20%D0%9A%D0%92%D0%90%D0%9D%D0%A2.pdf'
 output_file = pdf_file[:len(pdf_file)-3] + "txt"
 custom_config_rus = r'--oem 3 --psm 6 -c preserve_interword_spaces=0'
 extract_and_recognize_text(pdf_file, output_file, lang='rus+eng')
