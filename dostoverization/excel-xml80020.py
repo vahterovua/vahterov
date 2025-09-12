a8125163a81122c6b295cd2a4611eaf4213988e3
vahterovua <u9172778297@yandex.ru>
2025-09-12 09:15:07 +0300

    Update excel-xml80020.py

--- dostoverization/excel-xml80020.py
+++ dostoverization/excel-xml80020.py
@@ -29,3 +29,51 @@ else:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Получение списка всех файлов в текущей рабочей директории
+current_directory = os.getcwd()  # Текущий рабочий каталог
+all_files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
+
+if len(all_files) > 0:
+    st.subheader('Доступные файлы для скачивания:')
+    for file in all_files:
+        file_path = os.path.join(current_directory, file)
+        button_label = f"📥 {file}"
+        # Создаем кнопку для скачивания каждого файла
+        st.download_button(
+            label=button_label,
+            data=download_file(file_path),
+            file_name=file,
+            mime=None,  # Auto-detect MIME type based on the extension
+            key=file  # Уникальный ключ для каждой кнопки
+        )
+else:
+    st.info("Нет доступных файлов для скачивания.")
