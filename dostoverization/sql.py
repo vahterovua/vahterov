5dbbdb5dcde22228da0b8715761deaf0ea592002
vahterovua <u9172778297@yandex.ru>
2025-09-26 10:58:56 +0300

    Update sql.py

--- dostoverization/sql.py
+++ dostoverization/sql.py
@@ -5,34 +5,3 @@ from sqlalchemy import create_engine
 
 st.text('SQL запросы')
 
-# Создание подключения к базе данных PostgreSQL
-def create_connection():
-    try:
-        # Замените значения ниже на ваши реальные данные для подключения
-        engine = create_engine('postgresql://your_username:your_password@your_host/your_database_name')
-        return engine
-    except Exception as e:
-        print(f"Ошибка соединения с базой данных: {e}")
-        return None
-
-# Выполнение SQL-запросов и сохранение результата в датафрейме
-def run_query(query):
-    engine = create_connection()
-    if engine is not None:
-        try:
-            df = pd.read_sql_query(query, engine)
-            return df
-        except Exception as e:
-            print(f"Ошибка выполнения запроса: {e}")
-            return None
-    else:
-        return None
-
-# Основной сценарий программы
-if __name__ == "__main__":
-    sql_input = input("Введите ваш SQL-запрос:\n")  # Для интерактивного ввода в консоли
-    
-    result_df = run_query(sql_input)
-    if result_df is not None:
-        print(result_df)
-
